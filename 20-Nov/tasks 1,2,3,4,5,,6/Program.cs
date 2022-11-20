373c61116f96030c79d10da92c6e8e8710d4d526
MALEKOBEIDAT <115452586+MALEKOBEIDAT@users.noreply.github.com>
2022-11-20 05:00:59 -0800

    20-nov Tasks

---
+++ 20-Nov/tasks 1,2,3,4,5,,6/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks_1_2_3_4_5__6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+
+
+
+            //Task1
+
+
+            Console.WriteLine("Your Name");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine(Name);
+
+            //Task2
+
+            int x = 10;
+            double y = 2.5;
+            string a = "Malek";
+            char b = 'M';
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+
+
+            //Task3
+
+            string[] Cars = { "MArcsedies", "KIA", "Mazda" };
+            Console.WriteLine(Cars[0]);
+            Console.WriteLine(Cars[1]);
+            Console.WriteLine(Cars[2]);
+            Console.WriteLine("The Length Of Array = " + Cars.Length);
+
+
+
+
+
+            //Task4
+
+            Console.WriteLine("fName");
+
+
+            string fName = Console.ReadLine();
+
+            Console.WriteLine("SName");
+
+
+            string SName = Console.ReadLine();
+
+
+            Console.WriteLine("Year");
+
+
+            string Year = Console.ReadLine();
+
+
+
+
+
+
+
+            Console.WriteLine(fName + SName + Year);
+
+
+
+
+            //Task5
+
+            int[] Num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Element  = 1 : " + Num[0]);
+            Console.WriteLine("Element  = 2 : " + Num[1]);
+            Console.WriteLine("Element  = 4 : " + Num[2]);
+            Console.WriteLine("Element  = 5 : " + Num[3]);
+            Console.WriteLine("Element  = 6 : " + Num[4]);
+            Console.WriteLine("Element  = 7 : " + Num[5]);
+            Console.WriteLine("Element  = 8 : " + Num[6]);
+            Console.WriteLine("Element  = 9 : " + Num[7]);
+            Console.WriteLine("Element  = 10 : " + Num[8]);
+
+
+            int Sum = Num[0] + Num[1] + Num[2] + Num[3] + Num[4] + Num[5] + Num[6] + Num[7] + Num[8] + Num[9];
+
+            Console.WriteLine(Sum);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
