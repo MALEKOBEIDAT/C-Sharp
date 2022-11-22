cf534936e896b188d5079322c6fbcf16781c2982
MALEKOBEIDAT <115452586+MALEKOBEIDAT@users.noreply.github.com>
2022-11-22 02:32:40 -0800

    tasks

---
+++ 21-11-2022/Task-21-11/Task-21-11/Program.cs
@@ -0,0 +1,299 @@
+﻿
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace tasks_Mounday
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+            //Task1
+            //Console.WriteLine("Enter Num1");
+
+            //double Num1 =Convert.ToDouble( Console.ReadLine());
+
+            //Console.WriteLine("Enter Num2");
+
+            //double Num2 = Convert.ToDouble( Console.ReadLine());
+
+
+            //if (Num1 > Num2)
+            //{
+            //    Console.WriteLine(Num2);
+            //}
+
+            //else if (Num1 < Num2)
+            //{
+            //    Console.WriteLine( "Small Number is = " + Num1);
+
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Eqal");
+
+            //}
+
+
+            //Task2
+
+
+            //Console.WriteLine("Enter Num1");
+
+            //double Num1 = Convert.ToDouble(Console.ReadLine());
+
+
+            //if (Num1 > 0)
+            //{
+            //    Console.WriteLine("+");
+            //}
+
+            //else if (Num1 < 0)
+            //{
+            //    Console.WriteLine("-");
+
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("0");
+
+            //}
+
+
+            //Task3
+
+            //Console.WriteLine("Enter Num1");
+
+            //double Num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Enter Num2");
+
+            //double Num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //Console.WriteLine("Enter Num3");
+
+            //double Num3 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+            //if (Num1 > Num2 && Num2 > Num3)
+            //{
+            //    Console.WriteLine(Num1);
+            //    Console.WriteLine(Num2);
+            //    Console.WriteLine(Num3);
+
+
+
+
+
+            //}
+
+            //else if (Num1 > Num2 && Num3 > Num2)
+
+            //{
+            //    Console.WriteLine(Num1);
+            //    Console.WriteLine(Num3);
+            //    Console.WriteLine(Num2);
+
+
+
+
+            //}
+
+            //else if (Num2 > Num1 && Num1 > Num3)
+            //{
+            //    Console.WriteLine(Num2);
+            //    Console.WriteLine(Num1);
+            //    Console.WriteLine(Num3);
+
+
+
+            //}
+
+            //else if (Num2 > Num3 && Num3 > Num1)
+            //{
+            //    Console.WriteLine(Num2);
+            //    Console.WriteLine(Num3);
+            //    Console.WriteLine(Num1);
+
+
+
+            //}
+
+
+
+            //else if (Num3 > Num1 && Num2 > Num1)
+            //{
+            //    Console.WriteLine(Num3);
+            //    Console.WriteLine(Num2);
+            //    Console.WriteLine(Num1);
+
+
+
+            //}
+
+            //else if (Num3 > Num1 && Num1 > Num2)
+            //{
+            //    Console.WriteLine(Num3);
+            //    Console.WriteLine(Num1);
+            //    Console.WriteLine(Num2);
+
+
+
+            //}
+
+
+
+
+
+
+            //Task4
+
+            Console.WriteLine("Enter Num1");
+
+            double Num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Num2");
+
+            double Num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Enter Num3");
+
+            double Num3 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (Num1 > Num2 && Num1 > Num3)
+            {
+                Console.WriteLine(Num1);
+
+
+            }
+
+            if (Num2 > Num1 && Num2 > Num3)
+            {
+                Console.WriteLine(Num2);
+
+
+            }
+
+            if (Num3 > Num1 && Num3 > Num2)
+            {
+                Console.WriteLine(Num3);
+
+            }
+
+
+            //Task5
+
+            //Console.WriteLine(" kilometers per hour: ");
+
+            //double kilometers = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+            //Console.WriteLine((kilometers * 0.62137119) + " miles per hour");
+
+
+
+
+
+
+
+            //Task6
+
+
+
+            //Console.WriteLine("Enter Hours");
+
+            //double Hours = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Enter Munits");
+
+            //double Munits = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine((Hours * 60 ) + Munits + " Munits");
+
+
+            //Task7
+
+
+
+            //Console.WriteLine("Enter Muntis");
+
+            //int Muntis = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //int h = Muntis%60;
+
+
+
+
+
+
+
+
+            //Console.WriteLine((Muntis / 60 ) +  " Hours"  + h +"Munits");
+
+
+
+
+            //Task8
+
+            //string[] Name = { "Malek", "Mohammad", "Ali", "Alaa", "Ahmadd" };
+
+            //Console.WriteLine(Name[0].Substring(0, 3));
+            //Console.WriteLine(Name[1].Substring(0, 3));
+            //Console.WriteLine(Name[2].Substring(0, 3));
+            //Console.WriteLine(Name[3].Substring(0, 3));
+            //Console.WriteLine(Name[04].Substring(0, 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    }
+
---
+++ 22-Nov/22-Nov/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_Nov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+
+            //int[] ARR = { 1, 7, 9, 45 };
+            //string[] arr2 = { "Str", "alex", "moh" };
+
+            //string[] arr3 = { "the", "fox", "over", "lazy", "dog", };
+
+            //Task2
+
+            //String[] fruits ={"Tomato", "Banana", "Watermelon"};
+            //for(int i =1; i<fruits.Length; i++)
+            //{
+            //    Console.WriteLine (i);
+
+            //}
+
+
+            //Task3
+            //String[] Food ={"Tomato", "Banana", "Watermelon"};
+            //String[] Sport ={"Tomato", "Banana", "Watermelon"};
+            //String[] Movie ={"Tomato", "Banana", "Watermelon"};
+            //foreach(string i in Food)
+            //{
+            //    Console.Write(i+ " ");
+            //}
+
+            //Console.WriteLine(" ");
+
+            //foreach (string e in Sport)
+            //{
+            //    Console.Write(e + " ");
+            //}
+
+            //Console.WriteLine(" ");
+
+
+            //foreach (string m in Movie)
+            //{
+            //    Console.Write(m + " ");
+            //}
+
+            //Task4
+
+            string[] M = Console.ReadLine().Split(',');
+
+            int[] P = new int[M.Length];
+
+            for (int i = 0; i < M.Length; i++)
+            {
+                P[i] = Convert.ToInt32(M[i]);
+
+            }
+
+            Console.WriteLine(P[0] + P[1] + P[2]);
+
+
+
+            //Task5.
+
+            //int sum = 0;
+
+            //for (int i=0; i<=100; i++)
+
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.WriteLine(i);
+            //        sum += i;
+            //    }
+
+
+
+            //}
+
+            //Console.WriteLine(sum);
+
+
+            //Task6
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*" + " ");
+            //    }
+
+            //    Console.WriteLine();
+
+
+
+            //}
+            //int sum = 1;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write(sum + " " );
+            //        sum++;
+            //    }
+
+            //    Console.WriteLine();
+        }
+    }
+}
